b81ed207723151c27702e90a57f0618cb8a8b128
jadzeino <ahmad.zeino@gmail.com>
2021-05-22 13:21:28 +0200

    Update MissingNumber.js

--- MissingNumber.js
+++ MissingNumber.js
@@ -1,4 +1,7 @@
 /*
+PermMissingElem
+Find the missing element in a given permutation.
+
 An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
 Your goal is to find that missing element.
 Write a function:
