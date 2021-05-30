2c0ce616719900d722f2c163d70e18c5a7081343
jadzeino <ahmad.zeino@gmail.com>
2021-05-30 12:30:14 +0200

    Create MaxCountersOptimized.js
    You are given N counters, initially set to 0, and you have two possible operations on them:
    
    increase(X) − counter X is increased by 1,
    max counter − all counters are set to the maximum value of any counter.
    A non-empty array A of M integers is given. This array represents consecutive operations:
    
    if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
    if A[K] = N + 1 then operation K is max counter.
    For example, given integer N = 5 and array A such that:
    
        A[0] = 3
        A[1] = 4
        A[2] = 4
        A[3] = 6
        A[4] = 1
        A[5] = 4
        A[6] = 4
    the values of the counters after each consecutive operation will be:
    
        (0, 0, 1, 0, 0)
        (0, 0, 1, 1, 0)
        (0, 0, 1, 2, 0)
        (2, 2, 2, 2, 2)
        (3, 2, 2, 2, 2)
        (3, 2, 2, 3, 2)
        (3, 2, 2, 4, 2)
    The goal is to calculate the value of every counter after all operations.
    
    Write a function:
    
    function solution(N, A);
    
    that, given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
    
    Result array should be returned as an array of integers.
    
    For example, given:
    
        A[0] = 3
        A[1] = 4
        A[2] = 4
        A[3] = 6
        A[4] = 1
        A[5] = 4
        A[6] = 4
    the function should return [3, 2, 2, 4, 2], as explained above.
    
    Write an efficient algorithm for the following assumptions:
    
    N and M are integers within the range [1..100,000];
    each element of array A is an integer within the range [1..N + 1].

---
+++ MaxCountersOptimized.js
@@ -0,0 +1,39 @@
+/*
+  You are given N counters, initially set to 0, and you have two possible operations on them:
+  
+  • increase(X) − counter X is increased by 1,
+  • max counter − all counters are set to the maximum value of any counter.
+  
+  A non-empty zero-indexed array A of M integers is given. This array represents consecutive operations:
+  
+  • if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+  • if A[K] = N + 1 then operation K is max counter.
+  
+  Write a function  that, given an integer N and a non-empty zero-indexed array A consisting of M integers, 
+  returns a sequence of integers representing the values of the counters.
+*/
+
+function solution(N,A){
+  //create the counters array and set it by defualt to 0  
+ let counters = new Array(N).fill(0)
+ console.log("counters ",counters)
+ console.log("A ",A)
+ //define max value for the counter, this will chnge based on the  operation and index
+ let max=0
+ //loop through the array, check the value,based on this change the max and the counter
+ for(var i =0 ; i<A.length;i++){
+     //check if the value bigger than N
+     if(A[i] === N+1){
+         //set all counters to the max value
+         counters.fill(max)
+     }else{
+         //define index, the current value -1
+         const index=A[i]-1
+         //add counter of this index by 1 . the increament operation 
+         counters[index]++
+         //re set the max value if the counter[index] is bigger than max
+         if (counters[index] > max) max = counters[index]         
+     }
+ }
+ return  counters
+}
